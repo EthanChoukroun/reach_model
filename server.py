7daa1e03121ef2c6bbc5eefb701e99d347c70631
Ethan Choukroun <echoukroun@seatgeek.com>
2024-10-15 01:44:55 -0400

    change

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def read_root():
 
 @app.route("/calculate_budget", methods=['POST'])
 def calculate_budget():
-    transactions = request.json.get('transactions')
+    transactions = request.json('transactions')
     if not transactions:
         return jsonify({"error": "No transactions provided"}), 400
     data = main.create_datasets(transactions)
